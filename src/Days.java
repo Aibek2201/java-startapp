63696656a03b0a85651016d91be369e346a18d13
Aibek2201 <aibekalmabekuly@gmail.com>
2023-11-05 23:57:38 +0600

    java variables

---
+++ src/Days.java
@@ -0,0 +1,16 @@
+public class Days {
+    public static void main(String[] args) {
+        int seconds = 86400;
+        int days = seconds / 86400;
+        seconds %= 86400;
+        int hours = seconds / 3600;
+        seconds %= 3600;
+        int minutes = seconds / 60;
+        seconds %= 60;
+
+        System.out.println("Days = " + days);
+        System.out.println("Hours = " + hours);
+        System.out.println("Minutes = " + minutes);
+        System.out.println("Seconds = " + seconds);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        int a = 55;
+        int b = 10;
+        int c = a / b;
+        System.out.println("c = " + c);
     }
 }
