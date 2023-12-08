cfd5024b97ad706f1b15e12dbbd33669d98c97da
Aibek2201 <aibekalmabekuly@gmail.com>
2023-12-09 00:22:39 +0600

    added Fightclub

---
+++ fightclub/src/com/javastart/fightclub/FightClub.java
@@ -0,0 +1,7 @@
+package com.javastart.fightclub;
+
+public class FightClub {
+    public String fight(Cat firstCat, Cat secondCat){
+        return null;
+    }
+}
--- fightclub/src/com/javastart/fightclub/Main.java
+++ fightclub/src/com/javastart/fightclub/Main.java
@@ -2,6 +2,12 @@ package com.javastart.fightclub;
 
 public class Main {
     public static void main(String[] args) {
+        Cat manuCat = new Cat();
+        manuCat.setName("Manu");
+        manuCat.setWeight(3);
 
+        Cat sulCat = new Cat();
+        sulCat.setName("Sulu");
+        sulCat.setWeight(2);
     }
 }
