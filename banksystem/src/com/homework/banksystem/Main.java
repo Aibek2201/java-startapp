a03a9cf37e8f3335ae8e1351d08a212d30a008a5
Aibek2201 <aibekalmabekuly@gmail.com>
2023-12-11 00:03:15 +0600

    added paymentService

--- banksystem/src/com/homework/banksystem/Main.java
+++ banksystem/src/com/homework/banksystem/Main.java
@@ -1,7 +1,19 @@
 package com.homework.banksystem;
 
+import com.homework.banksystem.entity.Account;
+import com.homework.banksystem.entity.Bill;
+import com.homework.banksystem.entity.Person;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Person loriPerson = new Person("Lori", "+77015689546", "Jon");
+        Bill loriBill = new Bill(10000);
+        Account loriAccount = new Account(loriPerson, loriBill);
+
+        Person martinPerson = new Person("Martin", "+77472659584", "Doe");
+        Bill martinBill = new Bill(8000);
+        Account martinAccount = new Account(martinPerson, martinBill);
+
     }
 }
---
+++ banksystem/src/com/homework/banksystem/service/PaymentService.java
@@ -0,0 +1,19 @@
+package com.homework.banksystem.service;
+
+import com.homework.banksystem.entity.Account;
+import com.homework.banksystem.entity.Bill;
+
+public class PaymentService {
+
+    public void pay(Account account, int amount){
+        Bill bill = account.getBill();
+        if (bill.getAmount() < amount){
+            System.out.println("Платеж невозможен, недостаточно средств");
+        } else {
+            int currentBillAmount = bill.getAmount();
+            System.out.println("Платеж обрабатывается, текущее состояние счета: " + currentBillAmount);
+            bill.setAmount(currentBillAmount - amount);
+            System.out.println("Платеж выполнен, новое состояние счета: " + bill.getAmount());
+        }
+    }
+}
